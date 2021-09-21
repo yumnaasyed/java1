aba1c57491a17d69b4bfb85d4bd5e89fac1b041a
yumnasyed <yumnasyed@localhost>
2021-09-21 09:29:56 -0400

    k

--- firstJava/src/introJava/Java.java
+++ firstJava/src/introJava/Java.java
@@ -3,7 +3,8 @@ package introJava;
 public class Java {
 	
 	public static void main(String[] args) {
-		System.out.println("hello");
-	}
+		System.out.println("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");
 
+	}
+	
 }
---
+++ firstJava/src/introJava/test.java
@@ -0,0 +1,63 @@
+package introJava;
+
+public class test {
+	
+	public static void main(String[] args) {
+		System.out.println("Where are you from?");
+		System.out.println();
+		System.out.println("I am from Shrewsbury, MA. I have always lived here, and I went to an Islamic private elementary and middle school.");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Do you have siblings?");
+		System.out.println();
+		System.out.println("I have two siblings, and I am the youngest. My older sister graduated from St. Marks in 2018 and is now a senior at Babson College. My brother is a senior at St. Marks.");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Do you have a hobby?");
+		System.out.println();
+		System.out.println("I really enjoy reading. This summer I read a total of 15 books, which was below my goal. I have recently gotten into the classic, including Jane Austen, F. Scott Fitzgerald, Charles Dickens, etc. My favorite book so far is probably The Bell Jar by Sylvia Plath.");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("What is your favorite TV show/movie?");
+		System.out.println();
+		System.out.println("My favorite TV show is Gilmore Girls because I relate to the characters a lot. I think I am a lot like Paris Geller and Rory Gilmore, so it has become my comfort show. My favorite movie is Pride and Prejudice because I read the book afterwards and loved to compare the stories.");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("What is your favorite kind of food?");
+		System.out.println();
+		System.out.println("My favorite food cuisine is definitely mediterranean. I grew up eating a lot of it because my mom used to live in Saudi Arabia.");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("What kind of music do you listen to?");
+		System.out.println();
+		System.out.println("I do not specifically listen to one genre, but I do love Taylor Swift. After listening to all her albums, Folklore, Speak Now, and Red have become my favorite.");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("What is your favorite subject?");
+		System.out.println();
+		System.out.println("My favorite subject is either English or Computer Science. I love literature, so writing has always been fun for me.");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("What is a fun fact about you?");
+		System.out.println();
+		System.out.println("I am a canadian citizen because all of my cousins live there. I usually visit there three to four times a year.");
+		
+	}
+
+}
+
+# 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
