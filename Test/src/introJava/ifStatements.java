3cd159be92eb8e349afa536b0b3f896be9c0fc00
yumnasyed <yumnasyed@localhost>
2021-09-24 11:58:48 -0400

    :)

---
+++ Test/src/introJava/ifStatements.java
@@ -0,0 +1,26 @@
+package introJava;
+
+public class ifStatements {
+	
+	public static void main(String[] args) {
+		
+		int x = 6;
+		
+		if (x == 3) {
+			
+			System.out.println("hi");		
+		}
+		
+		else if (3 > x && x > 8) {
+			
+			System.out.println("hey");
+		}
+		
+		else {
+			
+			System.out.println("hello");
+		}
+	
+	}
+	
+}
