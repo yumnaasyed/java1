84c44cbad1292378ed9a9d213626f5be84a94739
yumnasyed <yumnasyed@localhost>
2021-09-25 22:50:09 -0400

    if statements

--- Test/src/introJava/ifStatements.java
+++ Test/src/introJava/ifStatements.java
@@ -2,25 +2,94 @@ package introJava;
 
 public class ifStatements {
 	
-	public static void main(String[] args) {
+	public void positive(int x) {
 		
-		int x = 6;
-		
-		if (x == 3) {
-			
-			System.out.println("hi");		
+		if (x > 0) {
+			System.out.println("positive");
 		}
 		
-		else if (3 > x && x > 8) {
-			
-			System.out.println("hey");
+		else if (x < 0) {
+			System.out.println("negative");
 		}
 		
 		else {
-			
-			System.out.println("hello");
+			System.out.println("zero");
+		}
+	}
+	
+	
+	public void evenOdd(int x) {
+		
+		if (x % 2 == 0) {
+				System.out.println("even");
+		}
+		
+		else {
+				System.out.println("odd");
+		}
+	}
+	
+	
+	public void upperLower(char x) {
+		
+		if (x >= 65 && x <= 90) {
+			System.out.println("uppercase");
 		}
 	
+		else if (x >= 97 && x <= 122) {
+			System.out.println("lowercase");
+		}
+		
+		else {
+			System.out.println("neither");
+			
+		}
+	}
+	
+	public void multiple(int x) {
+		
+		if (x % 10 == 0) {
+			System.out.println("multiple of 10");
+		}
+		
+		else {
+			System.out.println("not multiple of 10");
+		}
+		
+	}
+	
+	public void biggest(double a, double b, double c) {
+		
+		if (a > b && a > c) {
+			System.out.println(a);
+		}
+		
+		else if (b > c && b > a) {
+			System.out.println(b);
+		}
+		
+		else {
+			System.out.println(c);
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+
+		ifStatements runner = new ifStatements();
+		
+		runner.positive(-100);
+		
+		runner.evenOdd(26);
+		
+		runner.upperLower('A');
+		
+		runner.multiple(65);
+		
+		runner.biggest(50, 7, 0.5);
+		
 	}
 	
 }
+	
