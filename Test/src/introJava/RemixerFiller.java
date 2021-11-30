74298bf44ac33fca58688bc6e0b4ae7b96659cff
yumnasyed <yumnasyed@localhost>
2021-11-30 09:44:44 -0500

    remix

---
+++ Test/src/introJava/RemixerFiller.java
@@ -0,0 +1,88 @@
+package introJava;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class RemixerFiller {
+
+	private String[] lyrics;
+	private String fileName = "Song";	// this should match your txt file
+	
+	//private String oldWord = "all";
+	//private String newWord = "not";
+	
+	
+	private String[] wordsToReplace;	// for part 2, if you finish the main project
+	private String[] newWords;
+	
+	public void defArrs() {
+		wordsToReplace = new String[] {"all", "too", "well"};
+		newWords = new String[] {"not", "that", "well"};
+	}
+ 	public void remix() {
+		defArrs();
+		// your code here
+		
+		for (int i = 0; i < lyrics.length; i++) {
+			for (int j = 0; j < wordsToReplace.length; j++) {
+				
+				if (lyrics[i].equals(wordsToReplace[j])) {
+				lyrics[i] = (newWords[j]);
+				}
+			}
+		}
+		
+	}
+	
+	// DON'T TOUCH THE BELOW CODE //
+	
+	public RemixerFiller() {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(fileName));
+			ArrayList<String> tempLyrics = new ArrayList<String>();
+			for (String line = in.readLine(); line != null; line = in.readLine()) {
+				
+				String[] words = line.split(" ");
+				for (String w : words) {
+					tempLyrics.add(w.toLowerCase().replaceAll("^[^a-zA-Z0-9\\s]+|[^a-zA-Z0-9\\s]+$", ""));
+				}
+				tempLyrics.add("\n");
+			}
+			in.close();
+			lyrics = new String[tempLyrics.size()];
+			for (int i = 0; i < tempLyrics.size(); i++) {
+				lyrics[i] = tempLyrics.get(i);
+			}
+			
+
+			remix();
+			
+			if (fileName.substring(fileName.length()-4).equals(".txt")) {
+				fileName = fileName.substring(0, fileName.length()-4);
+			}
+			
+			BufferedWriter out = new BufferedWriter(new FileWriter(fileName + "_remixed"));
+			
+			for (String s : lyrics) {
+				out.write(s + (s.equals("\n") ? "" : " "));
+			}
+			out.close();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+			System.exit(1);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new RemixerFiller();
+	}
+}
