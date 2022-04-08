8ecc0d08e18c8691d624c4b069ac10113601ae73
yumnasyed <yumnasyed@localhost>
2022-04-08 11:33:49 -0400

    relative max problem

---
+++ Test/src/introJava/finalStringsClasswork.java
@@ -0,0 +1,23 @@
+package introJava;
+
+import java.util.ArrayList;
+
+public class finalStringsClasswork {
+	
+	public static ArrayList<Integer> relativeMax(double[] ds) {
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		for (int i = 1; i < ds.length-1; i++) {
+			if (ds[i-1] < ds[i] && ds[i+1] < ds[i]) {
+				a.add(i);
+			}
+		}
+		return a;
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(relativeMax(new double[] {5.5, 6.3, 4.4, 9.2, 7.7}));
+
+	}
+	
+}
