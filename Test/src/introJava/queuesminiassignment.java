8177b0a8f0f378dd902398005dcfefbac11966f0
yumnasyed <yumnasyed@localhost>
2022-12-15 23:59:24 -0500

    zeros and nines

--- Test/src/introJava/queuesminiassignment.java
+++ Test/src/introJava/queuesminiassignment.java
@@ -23,16 +23,46 @@ public class queuesminiassignment {
 		}
 	}
 	
-	public static void zerosAndNines(int n) {
-		System.out.println("When n is "+n);
-		Queue<String> q=new LinkedList<>();//a queue
-		//your code here
+	static List<String> vec = new LinkedList<String>();
+	
+	public static void zerosAndNines() {
+		
+		//System.out.println("When n is "+n);
+		Queue<String> q=new LinkedList<>();
+		q.add("9");
+		
+		for (int i = 1000; i > 0; i-- ) {
+			String string1 = q.peek();
+            q.remove();
+            
+            vec.add(string1);
+            
+            String string2 = string1;
+            
+            q.add(string1 + "0");
+            q.add(string2 + "9");
+		}
+		
+
+	}
+	
+	public static String findSmallestMultiple(int n) {
+		
+		for (int i = 0; i < vec.size(); i++) {
+			
+			if (Integer.parseInt(vec.get(i)) % n == 0) {
+				return vec.get(i);
+			}
+		}
+		return "";
 	}
 	
 	public static void main(String[] args) {
 		// starbucks();
-		generateBinaryNumber(8);
-		// zerosAndNines(in.nextInt());
+		//generateBinaryNumber(8);
+		// zerosAndNines(7);
+		zerosAndNines();
+		System.out.println(findSmallestMultiple(7));
 		//in.close();
 	}
 
